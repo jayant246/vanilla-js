308e303bb2a9fc7151130dae5e0abc6c9e4a4334
jayant246 <jayant1217246@jmit.ac.in>
2024-03-08 00:14:13 +0530

    feat: web worker in js and their uses

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log('file loaded')
+/* here we have 4 buttons 
+1. to calculate the sum and simultaneously change the bg, but we are unable to do so if we have a heavy task ongoing
+2. so we shift the heavy task to the web worker
+3. rest 2 buttons are 2 fetch the data from the API
+*/
+const worker = new Worker("./worker.js");
+const preElem = document.querySelector('#pre');
+const TODO_URL = 'https://jsonplaceholder.typicode.com/todos';
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const countSum = () => {
+  for (let i = 0; i < 10000000000; i++) {}
+  console.log("task finished");
+  worker.postMessage("hey there");
+  // our main thread will 
+  worker.onmessage = (msg) => {
+    alert("msg from worker", msg.data);
+  };
+};
+
+const changeBg = () => {
+  if (document.body.style.background === "green") {
+    document.body.style.background = "blue";
+  } else document.body.style.background = "green";
+};
+
+const fetchTodos = () => {
+    worker.postMessage({actionType: 'FETCH', payload: TODO_URL});
+    worker.onmessage = (msg) => {
+        const {data} = msg;
+        preElem.textContent = JSON.stringify(data, null, 2);
+    }
+}
+
+const fetchPosts = () => {
+    worker.postMessage({actionType: 'FETCH', payload: POSTS_URL});
+    worker.onmessage = (msg) => {
+        const {data} = msg;
+        preElem.textContent = JSON.stringify(data, null, 2);
+    }
+}
---
+++ worker.js
@@ -0,0 +1,35 @@
+/* 
+1. self here represent the worker object
+2. in worker file the worker object is the global object i.e  self
+3. we can directly use postMessage or worker.postMessage
+4. onMessage is the event listener attach for listening all the postMessages for that worker
+5. web worker doesn't have access to the DOM but have access to 
+    - setTimeout & setInterval
+    - fetch API
+    - navigator API
+    - have their own thread, execution context and event loop
+*/
+
+self.onmessage = (msg) => {
+  console.log("msg received", msg.data);
+  const { actionType = "default" } = msg?.data;
+  switch (actionType) {
+    case "FETCH": {
+      const url = msg.data.payload;
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => postMessage(data))
+        .catch((err) => postMessage(err));
+      break;
+    }
+    default: {
+      let sum = 0;
+      console.log("max", Math.min());
+      for (let i = 0; i < 10000000000; i++) {
+        sum += 1;
+      }
+
+      postMessage(sum);
+    }
+  }
+};
